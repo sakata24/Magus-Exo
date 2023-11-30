9c322b014017e39a4bc3b6b5f4ec1e340dfc890c
silvak23 <silvak23@up.edu>
2023-11-30 01:12:06 -0800

    Selecting implemented

--- HUDs/SkillMenu.gd
+++ HUDs/SkillMenu.gd
@@ -1,5 +1,10 @@
 extends CanvasLayer
 
+@onready var Icon = preload("res://Resources/hud_elements/SkillIcon.tscn")
+@onready var SpellContainer = $Control/HBoxContainer/ColorRect2/LeftMargin/Left/SpellContainer
+@onready var Details = $Control/HBoxContainer/Right/RightContainer/DetailsMargin/SkillDetails
+@onready var EquipContainer = $Control/HBoxContainer/Right/RightContainer/EquipMargin/EquippedAbilities
+
 var dict
 
 
@@ -7,8 +12,9 @@ var dict
 func _ready():
 	var file = FileAccess.open("res://Resources/abilitysheet.txt", FileAccess.READ)
 	dict = UniversalSkills.get_skills()
-	print_dictionary()
+#	print_dictionary()
 	file.close()
+	_draw_icons()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,9 +22,34 @@ func _process(_delta):
 	pass
 
 
-
+func _draw_icons():
+	for n in dict:
+		var instance = Icon.instantiate()
+		instance.init(n)
+		instance.connect("equip", _equip_skill)
+		instance.connect("select", _select_skill)
+		SpellContainer.add_child(instance)
 
 
 func print_dictionary():
 	for n in dict:
 		print(UniversalSkills._get_ability(n))
+
+func _select_skill(skill:SkillIcon):
+	Details.get_node("PhotoContainer/DetailIcon").texture = skill.texture
+	var skillInfo = UniversalSkills._get_ability(skill.spell)
+	Details.get_node("name/Value").text = skillInfo.name
+	Details.get_node("cooldown/Value").text = str(skillInfo.cooldown)
+	Details.get_node("dmg/Value").text = str(skillInfo.dmg)
+	Details.get_node("element/Value").text = skillInfo.element
+	Details.get_node("lifetime/Value").text = str(skillInfo.lifetime)
+	Details.get_node("size/Value").text = str(skillInfo.size)
+	Details.get_node("speed/Value").text = str(skillInfo.speed)
+	Details.get_node("timeout/Value").text = str(skillInfo.timeout)
+	Details.get_node("type/Value").text = skillInfo.type
+
+
+func _equip_skill(skill:SkillIcon):
+	for n in EquipContainer.get_children():
+		if n.name == "":
+			n.set_icon(skill)
