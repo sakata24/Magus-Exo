7ab17c7df2da5a206faf79aa859828a8287ed16a
takaSa <sakata24@up.edu>
2024-01-04 06:35:11 -0800

    added repeating rooms and changed monster locations
    BUG WHEN CREATING ROOMS ALREADY ADJACENT TO 2 OLD ROOMS

--- Maps/Map.gd
+++ Maps/Map.gd
@@ -1,5 +1,23 @@
 extends Node2D
 
+signal edge_reached(edge, location)
 
 func _ready():
 	pass
+
+func _on_north_visibility_notifier_screen_entered():
+	$NorthVisibilityNotifier.queue_free()
+	emit_signal("edge_reached", 'N', global_position)
+
+func _on_south_visibility_notifier_screen_entered():
+	$SouthVisibilityNotifier.queue_free()
+	emit_signal("edge_reached", 'S', global_position)
+
+func _on_east_visibility_notifier_screen_entered():
+	$EastVisibilityNotifier.queue_free()
+	emit_signal("edge_reached", 'E', global_position)
+
+func _on_west_visibility_notifier_screen_entered():
+	$WestVisibilityNotifier.queue_free()
+	emit_signal("edge_reached", 'W', global_position)
+	
