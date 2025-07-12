84f20c6aed2534bfc974d5b0f36fa8cdbc215b68
takaSa <97773052+sakata24@users.noreply.github.com>
2025-07-12 16:15:54 -0700

    added tooltip option in menu
    fixed some layering issues as well

--- HUDs/SettingsUI.gd
+++ HUDs/SettingsUI.gd
@@ -2,7 +2,7 @@ extends CanvasLayer
 
 @onready var ConfirmPopup = preload("res://HUDs/ConfirmationPopup.tscn")
 # Enum of each setting from how it shows top to bottom
-enum SETTING_LIST {RESOLUTION, MASTER_VOL}
+enum SETTING_LIST {RESOLUTION, MASTER_VOL, TOOLTIPS_ENABLED}
 
 # Array of dictionaries to hold:
 	# If the setting is changed
@@ -13,10 +13,11 @@ func _ready() -> void:
 	# Initialize each setting with the node container as well as if it is changed
 	settingListArray.append({"changed_value" : null, "node" : $ContentContainer/VBoxContainer/MarginContainer/ScrollContainer/SettingTypeContainer/VideoOptionsMarginContainer/VBoxContainer/Resolution})
 	settingListArray.append({"changed_value" : null, "node" : $ContentContainer/VBoxContainer/MarginContainer/ScrollContainer/SettingTypeContainer/AudioOptionsMarginContainer2/VBoxContainer/MasterVolume})
-	
+	settingListArray.append({"changed_value" : null, "node" : $ContentContainer/VBoxContainer/MarginContainer/ScrollContainer/SettingTypeContainer/GameOptionsMarginContainer/VBoxContainer/Tooltips})
 	# Initialize the values of the UI to match the saved data
 	settingListArray[SETTING_LIST.RESOLUTION]["node"].get_node("OptionButton").selected = Settings.resolution
 	settingListArray[SETTING_LIST.MASTER_VOL]["node"].get_node("HSlider").value = Settings.master_volume
+	settingListArray[SETTING_LIST.TOOLTIPS_ENABLED]["node"].get_node("CheckBox").button_pressed = Settings.tooltips_enabled
 
 func _on_apply_button_pressed() -> void:
 	var inst = ConfirmPopup.instantiate()
@@ -40,6 +41,9 @@ func _setting_changes_accepted():
 		Settings.master_volume = settingListArray[SETTING_LIST.MASTER_VOL]["changed_value"]
 		# Apply the change
 		Settings.set_master_volume()
+	if settingListArray[SETTING_LIST.TOOLTIPS_ENABLED]["changed_value"] != null:
+		# Record the change in global settings
+		Settings.tooltips_enabled = settingListArray[SETTING_LIST.TOOLTIPS_ENABLED]["changed_value"]
 	
 	# Save data
 	Settings.save()
@@ -55,6 +59,9 @@ func _on_h_slider_value_changed(value: float) -> void:
 # Get the changed value for resolution
 func _on_resolution_option_button_item_selected(index: int) -> void:
 	settingListArray[SETTING_LIST.RESOLUTION].set("changed_value", index)
+
+func _on_tooltips_enabled_check_box_toggled(toggled_on: bool) -> void:
+	settingListArray[SETTING_LIST.TOOLTIPS_ENABLED].set("changed_value", toggled_on)
 
 # Get the changed value for master volume
 func _on_h_slider_drag_ended(value_changed: bool) -> void:
